9de1f2a2dfcac77c02bfe19c5ff77b058c02d640
aksafan <subzero_ck@ukr.net>
2025-03-21 19:29:10 -0500

    Add configs

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+import rateLimiter from "./rateLimiter.js";
+import db from "./db.js";
+
+export default {
+  rateLimiter,
+  db,
+};
---
+++ src/config/rateLimiter.js
@@ -0,0 +1,4 @@
+export default {
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  limit: 100, // limit each IP to 100 requests per windowMs
+};
