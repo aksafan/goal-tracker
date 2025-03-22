86efa1e36d318787974d91e9a695160a0613bb52
aksafan <subzero_ck@ukr.net>
2025-03-21 19:35:18 -0500

    Add security packages and ES6 fixes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const app = express();
-const cors = require('cors')
-const favicon = require('express-favicon');
-const logger = require('morgan');
+import express from "express";
+import cors from "cors";
+import favicon from "express-favicon";
+import logger from "morgan";
+import { rateLimit } from "express-rate-limit";
+import config from "./config";
+import helmet from "helmet";
+import mainRouter from "./routes/mainRouter.js";
+import xss from "xss-clean";
 
-const mainRouter = require('./routes/mainRouter.js');
+const app = express();
 
 // middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(express.static('public'))
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
+
+// extra security middlewares
+app.use(cors());
+app.use(rateLimit(config.rateLimiter));
+app.use(helmet());
+app.use(xss());
 
 // routes
-app.use('/api/v1', mainRouter);
+app.use("/api/v1", mainRouter);
 
 module.exports = app;
