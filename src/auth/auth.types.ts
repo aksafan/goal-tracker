34d94b22b5945c0dc97fff7c0bca8bb1429e736e
IrynaSt <irinastoetski13@gmail.com>
2025-05-03 22:29:43 -0500

    added user to login response

--- src/auth/auth.types.ts
+++ src/auth/auth.types.ts
@@ -1,15 +1,19 @@
+type User = {
+  id: string;
+  email: string;
+  name: string;
+  created_at: Date;
+};
+
+
 export type RegisterResponse = {
   access_token: string;
   refresh_token: string;
-  user: {
-    id: string;
-    email: string;
-    name: string;
-    created_at: Date;
-  };
+  user: User;
 };
 
 export type LoginResponse = {
   access_token: string;
   refresh_token: string;
+  user: User;
 };
